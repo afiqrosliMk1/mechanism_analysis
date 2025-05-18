3f92ab238515b9658048fd50c386aaa25b464a86
afiq rosli <afiq2381@gmail.com>
2025-05-18 23:07:58 +0800

    add if..else to handle case for class 2 and 4

--- Fourbar_Position_Analysis_with_non_grashoff.m
+++ Fourbar_Position_Analysis_with_non_grashoff.m
@@ -8,40 +8,63 @@
 
 % prepare workspace
 clear variables; close all; clc;
-a = 0.17;               % crank length (m)
-b = 0.2;               % coupler length (m)
-c = 0.13;               % rocker length (m)
-d = 0.22;              % ground link length (m)
-p = 0.150;               % length from B to P (m)
-gamma = 20 * pi / 180; % angle between BP and coupler (radian)
+% crank length (m)
+a = 0.17;
+% coupler length (m)
+b = 0.2;
+% rocker length (m)
+%c = 0.13; 
+c = 0.22;
+% ground link length (m)
+%d = 0.22;
+d = 0.13;
+% length from B to P (m)
+p = 0.150; 
+% angle between BP and coupler (radian)
+gamma = 20 * pi / 180; 
 
-% if grashoff condition met, grashoff = 0
-% if non-grashoff, grashoff = 1
-grashoff = 0;
-%ccw = 0, cw = 1;
-cw = 0;
+% if grashoff condition met, grashoff = true, otherwise false
+grashoff = true;
+% linkage class. initialise to 0.
+class = 0;
+% driving link clocwise or counterclockwise
+ccw = true;
+
 % check for grashoff condition
 [S, L, P, Q] = slpq(a , b, c, d);
-if S + L < P + Q 
-    fprintf("grashoff\n")
+if S + L < P + Q
+    if S == d
+        % short link is ground
+        class = 1;
+    elseif S == a
+        % short link is crank
+        class = 2;
+    elseif S == b
+        % short link is coupler
+        class = 3;
+    elseif S == c
+        % short link is rocker
+        class = 4;
+    end
+    fprintf("grashoff class %d\n", class)
     theta2_min = 0;
     theta2_max = 2*pi;
 else
     if L == a
         fprintf("non grashoff class 7 - longest link is crank (link ""a"")\n")
-        grashoff = 2;
+        class = 7;
     elseif L == b
         fprintf("non grashoff class 8&9 - longest link is coupler (link ""b"")\n")
-        grashoff = 3;
-        cw = 1;
+        class = 8;
+        cw = false;
         theta2_max = acos((a^2 + d^2 - (b - c)^2) / (2*a*d));
         theta2_min = -theta2_max;
     elseif L == c
         fprintf("non grashoff class 10 - longest link is rocker (link ""c"")\n")
-        grashoff = 4;
+        class = 10;
     elseif L == d
         fprintf("non grashoff class 5&6 - longest link is ground (link ""d"")\n")
-        grashoff = 1;
+        class = 5;
         theta2_max = acos((a^2 + d^2 - (b + c)^2) / (2*a*d));
         theta2_min = -theta2_max;
     end
@@ -68,52 +91,76 @@ xP = zeros(2, N);
 
 % main loop
 for i = 1:N
-    if cw == 0
+    if ccw == true
+        % if driving link rotates ccw, then theta2 takes positive angle
         theta2(i) = (i - 1) * (theta2_max - theta2_min) / (N - 1) + theta2_min;
-    elseif cw == 1
+    elseif ccw == false
+        % if driving link rotates cw, then theta2 takes negative angle
         theta2(i) = -1 * (i - 1) * (2*pi - (theta2_max - theta2_min)) / (N - 1) + theta2_min;
     end
   
-    %r = d - a * cos(theta2(i));
-    r = d + c * cos(theta2(i));
-    %s = a * sin(theta2(i));
-    s = c * sin(theta2(i));
-    
+    if class == 2
+        r = d - a * cos(theta2(i));
+        s = a * sin(theta2(i));
+    elseif class == 4
+        r = d + c * cos(theta2(i));
+        s = c * sin(theta2(i));
+    else
+        fprintf("class other than 2 and 4 not coverted.. return")
+        return
+    end
+
     f = sqrt(r^2 + s^2);
     
     % open configuration
-    %cos_val = (b^2 + c^2 - f^2) / (2*b*c);
-    cos_val = (a^2 + b^2 - f^2) / (2*a*b);
+    if class == 2
+        cos_val = (b^2 + c^2 - f^2) / (2*b*c);
+    elseif class == 4
+        cos_val = (a^2 + b^2 - f^2) / (2*a*b);
+    end
+    
     % Clamp to [-1, 1]. Important to handle floating point error. otherwise
     % delta will give you 0 + 2.107342425544702e-08i for theta2_min angle.
     cos_val = min(1, max(-1, cos_val));   
     delta = acos(cos_val);
     
-    % g = b - c * cos(delta);
-    % h = c * sin(delta);
-    g = b - a * cos(delta);
-    h = a * sin(delta);
-    
-    %changing the "x" to negative somehow works. but why?
-    theta3_prime(i) = atan2((h*r - g*s), (g*r + h*s));
-    theta3(i) = pi - theta3_prime(i);
-    
-    %theta4(i) = theta3(i) + delta;
-    theta4(i) = theta3(i) - delta;
-    
+    if class == 2
+        g = b - c * cos(delta);
+        h = c * sin(delta);
+    elseif class == 4
+        g = b - a * cos(delta);
+        h = a * sin(delta);
+    end
+
+    if class == 2
+        theta3(i) = atan2((h*r - g*s), (g*r + h*s));
+        theta4(i) = theta3(i) + delta;
+    elseif class == 4
+        % class 4 is the mirror of class 2
+        theta3_prime(i) = atan2((h*r - g*s), (g*r + h*s));
+        theta3(i) = pi - theta3_prime(i);
+        theta4(i) = theta3(i) - delta;
+    end
+
     [e2, n2] = UnitVector(theta2(i));
     [e3, n3] = UnitVector(theta3(i));
     [e4, n4] = UnitVector(theta4(i));
-    %[eBP, nBP] = UnitVector(theta3(i) + gamma);
-    [eCP, nCP] = UnitVector(theta3(i) - gamma);
-    
-    %xB(:, i) = FindPos(x0, c, e2);
-    xB(:, i) = FindPos(xD, a, e4);
-    %xC(:, i) = FindPos(xD, a, e4);
-    xC(:, i) = FindPos(x0, c, e2);
-    %xP(:, i) = FindPos(xB(:, i), p, eBP);
-    xP(:, i) = FindPos(xC(:, i), p, eCP);
-    
+
+    if class == 2
+        [eBP, nBP] = UnitVector(theta3(i) + gamma);
+    elseif class == 4
+        [eCP, nCP] = UnitVector(theta3(i) - gamma);
+    end
+
+    if class == 2
+        xB(:, i) = FindPos(x0, c, e2);
+        xC(:, i) = FindPos(xD, a, e4);
+        xP(:, i) = FindPos(xB(:, i), p, eBP);
+    elseif class == 4
+        xB(:, i) = FindPos(xD, a, e4);
+        xC(:, i) = FindPos(x0, c, e2);
+        xP(:, i) = FindPos(xC(:, i), p, eCP);
+    end
 
 end
 
@@ -126,12 +173,16 @@ plot(xP(1, :), xP(2, :));
 axis equal
 
 %initialise linkage plot
-%h1 = plot([x0(1), xB(1, 1), xC(1, 1)], [x0(2), xB(2, 1), xC(2, 1)]);
-h1 = plot([x0(1), xC(1, 1), xB(1, 1)], [x0(2), xC(2, 1), xB(2, 1)]);
-%h2 = plot([xB(1, 1), xP(1, 1), xC(1, 1)], [xB(2, 1), xP(2, 1), xC(2, 1)]);
-h2 = plot([xC(1, 1), xP(1, 1), xB(1, 1)], [xC(2, 1), xP(2, 1), xB(2, 1)]);
-%h3 = plot([xD(1), xC(1, 1)], [xD(2), xC(2, 1)]);
-h3 = plot([xD(1), xB(1, 1)], [xD(2), xB(2, 1)]);
+if class == 2
+    h1 = plot([x0(1), xB(1, 1), xC(1, 1)], [x0(2), xB(2, 1), xC(2, 1)]);
+    h2 = plot([xB(1, 1), xP(1, 1), xC(1, 1)], [xB(2, 1), xP(2, 1), xC(2, 1)]);
+    h3 = plot([xD(1), xC(1, 1)], [xD(2), xC(2, 1)]);
+elseif class == 4
+    h1 = plot([x0(1), xC(1, 1), xB(1, 1)], [x0(2), xC(2, 1), xB(2, 1)]);
+    h2 = plot([xC(1, 1), xP(1, 1), xB(1, 1)], [xC(2, 1), xP(2, 1), xB(2, 1)]);
+    h3 = plot([xD(1), xB(1, 1)], [xD(2), xB(2, 1)]);
+end
+
 h4 = patch([xB(1, 1), xP(1, 1), xC(1, 1)], [xB(2, 1), xP(2, 1), xC(2, 1)], [0.5, 0.5, 1]);
 
 %initialise joint marker
@@ -152,13 +203,16 @@ while j <= N
     if ~isgraphics(fig)
         break
     end
-    %set(h1, 'XData', [x0(1), xB(1, j), xC(1, j)], 'YData', [x0(2), xB(2, j), xC(2, j)]);
-    set(h1, 'XData', [x0(1), xC(1, j), xB(1, j)], 'YData', [x0(2), xC(2, j), xB(2, j)]);
-    %set(h2, 'XData', [xB(1, j), xP(1, j), xC(1, j)], 'YData', [xB(2, j), xP(2, j), xC(2, j)]);
-    set(h2, 'XData', [xC(1, j), xP(1, j), xB(1, j)], 'YData', [xC(2, j), xP(2, j), xB(2, j)]);
-    %set(h3, 'XData', [xD(1), xC(1, j)], 'YData', [xD(2), xC(2, j)]);
-    set(h3, 'XData', [xD(1), xB(1, j)], 'YData', [xD(2), xB(2, j)]);
 
+    if class == 2
+        set(h1, 'XData', [x0(1), xB(1, j), xC(1, j)], 'YData', [x0(2), xB(2, j), xC(2, j)]);
+        set(h2, 'XData', [xB(1, j), xP(1, j), xC(1, j)], 'YData', [xB(2, j), xP(2, j), xC(2, j)]);
+        set(h3, 'XData', [xD(1), xC(1, j)], 'YData', [xD(2), xC(2, j)]);
+    elseif class == 4
+        set(h1, 'XData', [x0(1), xC(1, j), xB(1, j)], 'YData', [x0(2), xC(2, j), xB(2, j)]);
+        set(h2, 'XData', [xC(1, j), xP(1, j), xB(1, j)], 'YData', [xC(2, j), xP(2, j), xB(2, j)]);
+        set(h3, 'XData', [xD(1), xB(1, j)], 'YData', [xD(2), xB(2, j)]);
+    end
 
     %update joint marker
     set(h5, 'XData', [x0(1), xB(1, j), xC(1, j), xD(1), xP(1, j)], ...
@@ -179,19 +233,19 @@ while j <= N
     % if not grashoff, check if each extreme position has been reached. 
     % if yes, then change direction. 
     % if grashoff, use the logic that reset j = 1 every complete loop
-    if grashoff == 1
+    if class == 5
         if (j >= N && direction > 0) || (j <= 1 && direction < 0)
             direction = direction * -1;
         end
-    elseif grashoff == 2
+    elseif class == 6
         fprintf("under construction\n")
-    elseif grashoff == 3
+    elseif class == 7
         if (j >= N && direction > 0) || (j <= 1 && direction < 0)
             direction = direction * -1;
         end
-    elseif grashoff == 4
+    elseif class == 8
         fprintf("under construction\n")
-    elseif grashoff == 0
+    elseif grashoff == true
         if j == N & loop == true
             j = 1;
         end
